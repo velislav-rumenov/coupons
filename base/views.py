45d4c7f7ac7bd548863515aa585c9fbf7338d2ef
Velislav Rumenov <velislav.rumenov@g.austincc.edu>
2023-01-14 17:05:56 -0600

    returns JSON data if coupon located

--- base/views.py
+++ base/views.py
@@ -1,17 +1,15 @@
 from .models import Coupon
 from django.shortcuts import render, redirect
-from django.http import JsonResponse,HttpResponse
+from django.http import JsonResponse,HttpResponse, HttpResponseRedirect
 from django.core import serializers
 
 
 # Create your views here.
 def couponTest(request):
+    json_data = None
     if request.method == "POST":
-        calculate(request)
-    else:
-        return render(request, 'home.html')
-
-
-def calculate(request):
-    code = Coupon.objects.get(code__exact=request.POST["code"])
-    return HttpResponse("Works")
+        code = Coupon.objects.get(code__exact=request.POST["code"])
+        json_data = serializers.serialize('json', [code])
+        return JsonResponse(json_data)
+    context = {"json": json_data}
+    return render(request, 'home.html')
