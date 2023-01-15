b6be6f5a8bbe356cdba998784cdf6d69f2912d49
Velislav Rumenov <velislav.rumenov@g.austincc.edu>
2023-01-14 21:15:07 -0600

    implemented dynamic calculation (fixed vs percent) and added a /demo page th[Aat outputs a sample front end

--- base/urls.py
+++ base/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.couponTest),
+    path('', views.couponJson),
+    path('demo', views.couponApply)
 ]
--- base/views.py
+++ base/views.py
@@ -2,11 +2,12 @@ from .models import Coupon
 from django.shortcuts import render, redirect
 from django.http import JsonResponse, HttpResponse, HttpResponseRedirect
 from django.core import serializers
+import time
 import json
 
 
 # Create your views here.
-def couponTest(request):
+def couponJson(request):
     json_data = None
     if request.method == "POST":
         try:
@@ -17,4 +18,29 @@ def couponTest(request):
             json_data = serializers.serialize('json', [code])
             return JsonResponse(json.loads(json_data), safe=False)
     context = {"json": json_data}
+    return render(request, 'home.html')
+
+def couponApply(request):
+    if request.method == "POST":
+        try:
+            code = Coupon.objects.get(code__exact=request.POST["code"])
+        except:
+            return HttpResponse("Invalid Code")
+        else:
+            currentPrice = int(request.POST["price"])
+            isActive = code.isActive
+            if isActive:
+                if code.isFixed:
+                    newPrice = currentPrice - int(code.fixedDiscount)
+                    discount = f"${code.fixedDiscount}"
+                else:
+                    newPrice = currentPrice - currentPrice*int(code.percentDiscount)
+                    discount = f"{code.percentDiscount}%"
+                context={
+                    "code": code,
+                    "oldPrice": currentPrice,
+                    "newPrice": newPrice,
+                    "discount": discount
+                }
+            return render(request, "calculation.html", context=context)
     return render(request, 'home.html')
