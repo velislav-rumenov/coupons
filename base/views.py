ffdff8f885243351263e1a78e7aa928c8fd456b6
Velislav Rumenov <velislav.rumenov@g.austincc.edu>
2023-01-14 21:49:25 -0600

    added more fault checks and implemented use of validFrom and To dates for validation

--- base/views.py
+++ base/views.py
@@ -2,7 +2,7 @@ from .models import Coupon
 from django.shortcuts import render, redirect
 from django.http import JsonResponse, HttpResponse, HttpResponseRedirect
 from django.core import serializers
-import time
+import datetime as dt
 import json
 
 
@@ -30,17 +30,24 @@ def couponApply(request):
             currentPrice = int(request.POST["price"])
             isActive = code.isActive
             if isActive:
-                if code.isFixed:
-                    newPrice = currentPrice - int(code.fixedDiscount)
-                    discount = f"${code.fixedDiscount}"
+                print(dt.datetime.utcnow())
+                print(code.validFrom)
+                if code.validFrom < dt.datetime.utcnow().astimezone() < code.validTo:
+                    if code.isFixed:
+                        newPrice = currentPrice - int(code.fixedDiscount)
+                        discount = f"${code.fixedDiscount}"
+                    else:
+                        newPrice = currentPrice - currentPrice*int(code.percentDiscount)/100
+                        discount = f"{code.percentDiscount}%"
+                    context = {
+                        "code": code,
+                        "oldPrice": currentPrice,
+                        "newPrice": newPrice,
+                        "discount": discount
+                    }
                 else:
-                    newPrice = currentPrice - currentPrice*int(code.percentDiscount)
-                    discount = f"{code.percentDiscount}%"
-                context={
-                    "code": code,
-                    "oldPrice": currentPrice,
-                    "newPrice": newPrice,
-                    "discount": discount
-                }
+                    return HttpResponse("Coupon expired or not yet in effect")
+            else:
+                return HttpResponse("Code Inactive")
             return render(request, "calculation.html", context=context)
     return render(request, 'home.html')
