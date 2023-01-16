c1413edf97f38754b3d957d127245ae9bf7d90d0
Velislav Rumenov <velislav.rumenov@g.austincc.edu>
2023-01-16 16:10:01 -0600

    added territory field in home page, added a foreignKey field to coupon that is linked to territory

--- base/models.py
+++ base/models.py
@@ -3,7 +3,12 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 
 
 # Create your models here
+class Territory(models.Model):
+    name = models.CharField(max_length=25, unique=True)
+    zip = models.IntegerField(unique=True)
 
+    def __str__(self):
+        return self.name
 
 class Coupon(models.Model):
     code = models.CharField(max_length=50, unique=True)
@@ -13,11 +18,7 @@ class Coupon(models.Model):
     fixedDiscount = models.IntegerField(validators=[MinValueValidator(0)], default=0)
     isFixed = models.BooleanField(null=True)
     isActive = models.BooleanField(null=True)
+    territory = models.ForeignKey(Territory, on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
         return self.code
-
-
-class Territory(models.Model):
-    name = models.CharField(max_length=25, unique=True)
-    zip = models.IntegerField(unique=True)
